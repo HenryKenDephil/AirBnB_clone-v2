208720d95a7d508bfe326dce000bbfa5044e35a8
HenryKenDephil <95962384+HenryKenDephil@users.noreply.github.com>
2022-10-12 11:42:38 +0300

    MySQL setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,10 @@
+--a script that prepares MySQL serer for the project
+--creating  hbnb_test_db_database
+--proiding previleges to new user(hbnb_test)
+
+CREATE DATABASE IF NOT EXISTS 'hbnb_test_db';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+USE hbnb_test_db;
+GRANT ALL PREVILEGES ON hbnb_test_db.* To 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* To 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES
