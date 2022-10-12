ff027e86ae8f9b984784228937bc5ee244c5af6f
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-10-12 17:40:10 +0300

    setup_msql_test.sql commit

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,7 @@
---a script that prepares MySQL serer for the project
---creating  hbnb_test_db_database
---proiding previleges to new user(hbnb_test)
-
-CREATE DATABASE IF NOT EXISTS 'hbnb_test_db';
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-USE hbnb_test_db;
-GRANT ALL PREVILEGES ON hbnb_test_db.* To 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* To 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
+-- Script to create a MySQL server with the database hbnb_test_db.
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+GRANT USAGE ON *.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
