e32be0848256340902ad1491d10f93391890d535
HenryKenDephil <95962384+HenryKenDephil@users.noreply.github.com>
2022-10-12 11:50:44 +0300

    updating setup_mysql_test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,4 +7,4 @@ CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 USE hbnb_test_db;
 GRANT ALL PREVILEGES ON hbnb_test_db.* To 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* To 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES
+FLUSH PRIVILEGES;
