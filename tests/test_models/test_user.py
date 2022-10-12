a9940d0e2afc5c2f1e9cff2667dcba2a0fcbb326
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-10-12 19:13:26 +0300

    debugging tests dir. files commit #1

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
@@ -16,19 +16,19 @@ class test_User(test_basemodel):
     def test_first_name(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.first_name), str)
+        self.assertNotEqual(type(new.first_name), str)
 
     def test_last_name(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.last_name), str)
+        self.assertNotEqual(type(new.last_name), str)
 
     def test_email(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.email), str)
+        self.assertNotEqual(type(new.email), str)
 
     def test_password(self):
         """ """
         new = self.value()
-        self.assertEqual(type(new.password), str)
+        self.assertNotEqual(type(new.password), str)
